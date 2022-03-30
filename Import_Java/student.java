5ba8e3262a756b86b7d58aff78b2802104c5204e
Rajat <Rajatjoshi7535@gamil.com>
2022-03-30 14:57:53 +0530

    first commit

---
+++ Import_Java/student.java
@@ -0,0 +1,22 @@
+package Import_Java;
+
+public class student {
+    private int rollno;
+    private String name;
+    public void setRollno(int r)
+    {
+        rollno = r;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public int getRollno()
+    {
+        return (rollno);
+    }
+    public String getName()
+    {
+        return (name);
+    }
+}
