156b33b2200717380b4fdd1f11c60bf3b9adb765
Rajat <Rajatjoshi7535@gamil.com>
2022-03-30 18:59:50 +0530

    first commit

---
+++ Public/A.java
@@ -0,0 +1,8 @@
+package Public;
+
+public class A {
+    public void display()
+        {
+            System.out.println("Hello");
+        }
+    }
---
+++ Public/B.java
@@ -0,0 +1,9 @@
+package Public;
+
+public class B {
+    public static void main(String args[])
+    {
+        A obj = new A();
+        obj.display();
+    }
+}
