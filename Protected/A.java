6bcb2584ade9892fdf8202c6b5878692eecb382b
Rajat <Rajatjoshi7535@gamil.com>
2022-03-30 20:32:48 +0530

    first commit

---
+++ Protected/A.java
@@ -0,0 +1,8 @@
+package Protected;
+
+public class A {
+    protected void display()
+    {
+        System.out.println("Protected is running");
+    }
+}
---
+++ Protected/B.java
@@ -0,0 +1,9 @@
+package Protected;
+
+public class B extends A {
+    public static void main(String args[])
+    {
+        B obj = new B();
+        obj.display();
+    }
+}
